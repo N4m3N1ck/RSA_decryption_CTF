33ae1a18331f2730989f82e6b11efca078e372d0
N4meN1ck <138298706+N4meN1ck@users.noreply.github.com>
2023-07-05 15:19:33 +0100

    generate_primes.py: add generate_random_number_of_legnth(n)

--- RSA_encryption/generate_primes.py
+++ RSA_encryption/generate_primes.py
@@ -1,6 +1,5 @@
 import random
-length = int(input("Enter desired number length: "))
-count = int(input("Enter amount of numbers you want to generate: "))
+length = int(input("Enter the desired length of the number: "))
 
 
 def generate_random_number_of_length(n):
@@ -10,3 +9,24 @@ def generate_random_number_of_length(n):
         s += str(random.randint(0, 9))
     return int(s)
 
+
+def fermat_test(num):
+    for i in range(10):
+        a = random.randint(2, num-1)
+        if pow(a, num-1, num) != 1:
+            return False
+    return True
+
+
+def prime_test_slow(num):
+    for i in range(2, int(num**(1/2))+1):
+        if num % i == 0:
+            return False
+    return True
+
+
+n = generate_random_number_of_length(length)
+while not fermat_test(n):
+    n = generate_random_number_of_length(length)
+print(f"Very likely to be prime: {n}")
+# print(prime_test_slow(n))
